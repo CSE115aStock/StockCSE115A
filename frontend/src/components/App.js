7cb74cc7a896518eda3dcdacaac8426ff2017b63
Mihika Marathe <mihika.marathe@gmail.com>
2022-01-23 19:47:12 -0800

    npm package set up

---
+++ frontend/src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+/**
+ * Simple component with no state.
+ *
+ * @return {object} JSX
+ */
+function App() {
+  return (
+    <div>
+        Social Stock
+    </div>
+  );
+}
+
+export default App;
