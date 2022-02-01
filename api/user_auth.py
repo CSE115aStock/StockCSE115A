40d1bfa176d4683173bd0ea1b382eca2b807b78b
Ojas Upadhye <ojasupadhye@gmail.com>
2022-02-01 12:42:25 -0800

    Added portfolio managment and basic tests for user authenticatoin api

--- api/user_auth.py
+++ api/user_auth.py
@@ -4,93 +4,114 @@ import os
 import sys
 import re
 
-from datetime import  timedelta, datetime,timezone
+from datetime import timedelta, datetime, timezone
 from dotenv import load_dotenv, find_dotenv
 from pathlib import Path
 from psycopg2.extras import RealDictCursor
 from passlib.hash import bcrypt
-from flask import Flask, Blueprint, flash, redirect, url_for, render_template, current_app, request, jsonify
-from flask_jwt_extended import create_access_token,get_jwt,get_jwt_identity, \
-                               unset_jwt_cookies, jwt_required, JWTManager
+from flask import (
+    Flask,
+    Blueprint,
+    flash,
+    redirect,
+    url_for,
+    render_template,
+    current_app,
+    request,
+    jsonify,
+)
+from flask_jwt_extended import (
+    create_access_token,
+    get_jwt,
+    get_jwt_identity,
+    unset_jwt_cookies,
+    jwt_required,
+    JWTManager,
+)
 
-#load db connection config
-load_dotenv(find_dotenv())   
+# load db connection config
+load_dotenv(find_dotenv())
 
-HOST = os.getenv('HOST')
-PORT = os.getenv('PORT')
-DB_NAME = os.getenv('DB_NAME')
-USR = os.getenv('USR')
-PASSWORD = os.getenv('PASSWORD')
+HOST = os.getenv("HOST")
+PORT = os.getenv("PORT")
+DB_NAME = os.getenv("DB_NAME")
+USR = os.getenv("USR")
+PASSWORD = os.getenv("PASSWORD")
 
-#connect to user DB
-conn = psycopg2.connect(dbname=DB_NAME,user=USR,password=PASSWORD,host=HOST,port=PORT)
+# connect to user DB
+conn = psycopg2.connect(
+    dbname=DB_NAME, user=USR, password=PASSWORD, host=HOST, port=PORT
+)
 
-auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
+auth_bp = Blueprint("auth", __name__, url_prefix="/auth")
+
 
 def IsValidPassword(str):
-    regex = ("^(?=.*[a-z])(?=." +
-                     "*[A-Z])(?=.*\\d)" +
-                     "(?=.*[-+_!@#$%^&*., ?]).+$")
+    regex = "^(?=.*[a-z])(?=." + "*[A-Z])(?=.*\\d)" + "(?=.*[-+_!@#$%^&*., ?]).+$"
     # Compile the ReGex
     p = re.compile(regex)
 
     # If the string is empty
     # return false
-    if (len(str) < 8):
+    if len(str) < 8:
         return False
 
     # return true if string matches ReGex
-    if (re.search(p, str)):
+    if re.search(p, str):
         return True
     else:
         return False
 
-#Adds user to DB
-@auth_bp.route('/signup',methods=['GET','POST'])
+
+# Adds user to DB
+# INPUT: first name, last name, email, username, password
+@auth_bp.route("/signup", methods=["GET", "POST"])
 def AddUser():
     cur = conn.cursor()
 
-    if request.method == 'POST':
+    if request.method == "POST":
         f_name = request.json.get("first_name", None)
         l_name = request.json.get("last_name", None)
         email = request.json.get("email", None)
-        username = request.json.get("username",None)
+        username = request.json.get("username", None)
         p_word = request.json.get("password", None)
-        
+
         error = None
 
-        if not f_name:
-            error = 'First name is required.'
-        elif not l_name:
-            error = 'Last name is required.'
-        elif not email:
-            error = 'Email is required.'
-        elif not username:
-            error = 'Username is required.'
-        elif not p_word:
-            error = 'Password is required.'
-        
-        cur.execute('SELECT * from users where username = %(usr)s', {'usr': username})
+        if not f_name or len(f_name) == 0:
+            error = "First name is required."
+        elif not l_name or len(l_name) == 0:
+            error = "Last name is required."
+        elif not email or len(email) == 0:
+            error = "Email is required."
+        elif not username or len(username) == 0:
+            error = "Username is required."
+        elif not p_word or len(p_word) == 0:
+            error = "Password is required."
+
+        cur.execute("SELECT * from users where username = %(usr)s", {"usr": username})
         if cur.fetchone() != None:
-            error = 'User is already registered.'
-        
+            error = "User is already registered."
+
         if IsValidPassword(p_word) == False:
-            error = 'Password not strong enough.'
-        
+            error = "Password not strong enough."
+
         if error == None:
-            cur.execute("INSERT INTO users VALUES (%s, %s, %s, %s, %s, %s)", (f_name, l_name,email,username,bcrypt.hash(p_word),'{}'))
+            cur.execute(
+                "INSERT INTO users VALUES (%s, %s, %s, %s, %s, %s)",
+                (f_name, l_name, email, username, bcrypt.hash(p_word), "{}"),
+            )
             conn.commit()
             current_app.logger.info("User %s has been created succesfully.", username)
-            return redirect(url_for('auth.login'))
+            return jsonify({"err_msg": "Account creation successful"})
 
         current_app.logger.error(error)
-        return jsonify({"err_msg":error})
-    #set template
+        return jsonify({"err_msg": error})
 
 
-#method for refreshing tokens reaching expiration
-#Taken from flask-jwt documentation example at
-#https://flask-jwt-extended.readthedocs.io/en/stable/refreshing_tokens/
+# method for refreshing tokens reaching expiration
+# Taken from flask-jwt documentation example at
+# https://flask-jwt-extended.readthedocs.io/en/stable/refreshing_tokens/
 @auth_bp.after_request
 def refresh_expiring_jwts(response):
     try:
@@ -104,62 +125,60 @@ def refresh_expiring_jwts(response):
         return response
 
 
-
-#Checks if user credentials match a user entry in the DB.
-#Returns user if correct username and password combination are passed
-#Returns false otherwise.
-@auth_bp.route("/login", methods=['GET','POST'])
+# Checks if user credentials match a user entry in the DB.
+# Returns user if correct username and password combination are passed
+# Returns false otherwise.
+@auth_bp.route("/login", methods=["GET", "POST"])
 def AuthenticateUser():
     cur = conn.cursor()
 
-    username = request.json.get("username", None)
-    p_word = request.json.get("password", None)
-    cur.execute('SELECT * from users where username = %(usr)s', {'usr': username})
-    user = cur.fetchone()
-    error = None
+    if request.method == "POST":
+        username = request.json.get("username", None)
+        p_word = request.json.get("password", None)
+        cur.execute("SELECT * from users where username = %(usr)s", {"usr": username})
+        user = cur.fetchone()
+        error = None
 
-    if not user:
-        error = 'Invalid username'
-    else:    
-        hashed_pword = user[4]
-        if not check_password(p_word,hashed_pword):
-            error = 'Invalid password'
-        
-    if error != None:       
-        current_app.logger.info("Succesful login by user: %s", user[3])
+        if not user:
+            error = "Invalid username"
+        else:
+            hashed_pword = user[4]
+            if not check_password(p_word, hashed_pword):
+                error = "Invalid password"
 
-        #set redirect url to home page
+        if error == None:
+            current_app.logger.info("Succesful login by user: %s", user[3])
 
-        #set identity based on user email
-        access_token = create_access_token(identity=user[2])
-        return jsonify(access_token=access_token)
+            # set identity based on user email
+            access_token = create_access_token(identity=user[2])
+            return jsonify(access_token)
 
-        
-    current_app.logger.error(error)
-    return jsonify({"err_msg":error}), 401
-    #set template
+        current_app.logger.error(error)
+        return jsonify({"err_msg": error}), 401
 
 
-#endpoint for logging out. 
-#Taken from flask-jwt documentation example at
-#https://flask-jwt-extended.readthedocs.io/en/stable/refreshing_tokens/
-@auth_bp.route("/logout", methods=["POST"])
+# endpoint for logging out.
+# Taken from flask-jwt documentation example at
+# https://flask-jwt-extended.readthedocs.io/en/stable/refreshing_tokens/
+@auth_bp.route("/logout", methods=["GET", "POST"])
+@jwt_required()
 def logout():
     response = jsonify({"msg": "logout successful"})
     unset_jwt_cookies(response)
     return response
 
-#Checks if entered password matches hashed password in the db. 
+
+# Checks if entered password matches hashed password in the db.
 def check_password(plain_password, hashed_password):
     return bcrypt.verify(plain_password, hashed_password)
 
 
-#temp function to test connection with frontend
-#fetches sample user from the db
-@auth_bp.route("/user")
+# temp function to test connection with frontend
+# fetches sample user from the db
+@auth_bp.route("/user", methods=["GET","POST"])
+@jwt_required()
 def fetch_user():
     cur = conn.cursor(cursor_factory=RealDictCursor)
-    cur.execute("SELECT * FROM users where first_name='john'")
+    cur.execute("SELECT * FROM users where email=%s", (get_jwt_identity(),))
     usr = cur.fetchone()
     return jsonify(usr)
-
---
+++ api/user_auth_test.py
@@ -0,0 +1,147 @@
+import psycopg2
+import os
+import json
+import pytest
+from pathlib import Path
+
+from dotenv import load_dotenv, find_dotenv
+from unittest.mock import patch
+
+from api import app
+
+from flask_jwt_extended import (
+    create_access_token,
+    get_jwt,
+    get_jwt_identity,
+    unset_jwt_cookies,
+    jwt_required,
+    JWTManager,
+)
+
+# load db connection config
+load_dotenv(find_dotenv())
+
+HOST = os.getenv("HOST")
+PORT = os.getenv("PORT")
+DB_NAME = os.getenv("DB_NAME")
+USR = os.getenv("USR")
+PASSWORD = os.getenv("PASSWORD")
+
+
+# connect to user DB
+conn = psycopg2.connect(
+    dbname=DB_NAME, user=USR, password=PASSWORD, host=HOST, port=PORT
+)
+
+cur = conn.cursor()
+
+
+def test_AddUser():
+    with app.test_client() as c:
+        rv = c.post(
+            "/auth/signup",
+            json={
+                "first_name": "test_first",
+                "last_name": "test_last",
+                "email": "test.user@example.com",
+                "username": "test.user2",
+                "password": "Test@1234",
+            },
+        )
+        json_response = rv.get_json()
+
+        cur.execute("DELETE from users WHERE username='test.user2'")
+        conn.commit()
+
+        assert json_response["err_msg"] == "Account creation successful"
+
+
+def test_AddUser_weak_password():
+    with app.test_client() as c:
+        rv = c.post(
+            "/auth/signup",
+            json={
+                "first_name": "test_first",
+                "last_name": "test_last",
+                "email": "t1@example.com",
+                "username": "test.user2",
+                "password": "Test1234",
+            },
+        )
+        json_response = rv.get_json()
+
+        cur.execute("DELETE from users WHERE username='test.user2'")
+        conn.commit()
+
+        assert json_response["err_msg"] == "Password not strong enough."
+
+
+def test_AddUser_missing_field():
+    with app.test_client() as c:
+        rv = c.post(
+            "/auth/signup",
+            json={
+                "first_name": "test_first",
+                "last_name": "test_last",
+                "email": "",
+                "username": "test.user2",
+                "password": "Test@1234",
+            },
+        )
+        json_response = rv.get_json()
+
+        assert json_response["err_msg"] == "Email is required."
+
+
+def test_AddUser_already_registered():
+    with app.test_client() as c:
+        rv = c.post(
+            "/auth/signup",
+            json={
+                "first_name": "test_first",
+                "last_name": "test_last",
+                "email": "test@example.com",
+                "username": "test.user1",
+                "password": "Test@1234",
+            },
+        )
+        json_response = rv.get_json()
+
+        assert json_response["err_msg"] == "User is already registered."
+
+
+def test_AuthenticateUser():
+    with app.test_client() as c:
+        rv = c.post(
+            "/auth/login", json={"username": "test.user1", "password": "Test@1234"}
+        )
+        json_response = rv.get_json()
+        assert "err_msg" not in json_response
+
+
+def test_AuthenticateUser_bad_password():
+    with app.test_client() as c:
+        rv = c.post(
+            "/auth/login", json={"username": "test.user1", "password": "Test1234"}
+        )
+        json_response = rv.get_json()
+        assert json_response["err_msg"] == "Invalid password"
+
+
+def test_AuthenticateUser_bad_username():
+    with app.test_client() as c:
+        rv = c.post(
+            "/auth/login", json={"username": "test.use1", "password": "Test@1234"}
+        )
+        json_response = rv.get_json()
+        assert json_response["err_msg"] == "Invalid username"
+
+
+@patch("flask_jwt_extended.view_decorators.verify_jwt_in_request")
+def test_logout(mock_jwt):
+    with app.test_client() as c:
+        with app.app_context():
+            access_token = create_access_token("john@mail.com")
+            headers = {"Authorization": "Bearer {}".format(access_token)}
+            json_response = c.get("/auth/logout", headers=headers)
+            assert json_response != None
