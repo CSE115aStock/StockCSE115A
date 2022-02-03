f24d2f205de4e96bcdfdd4470236afc709e58cbd
nreusken <nreusken@ucsc.edu>
2022-02-03 15:28:16 -0800

    Dashboard and portfolio should be able to pull the data from db

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -17,7 +17,8 @@ import TableRow from '@mui/material/TableRow';
 import { createTheme } from '@mui/material';
 import Chart from './Charts/MACDchart';
 import { getData } from "./Charts/utils";
-import { TypeChooser } from "react-stockcharts/lib/helper";
+import { useState, useEffect } from 'react';
+
 
 
 const worth = 2.5;
@@ -95,112 +96,145 @@ class ChartComponent extends React.Component {
 }
 
 export default function Dashboard() {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2} >
-        <Grid item xs={8}>
-            <Card >
-                <CardContent>
-                <Typography gutterBottom variant="h6" component="div" color="primary">
-                    Today
-                </Typography>
-                <ChartComponent />
-                </CardContent>
-            </Card>
-        </Grid>
-        <Grid item xs={4}>
-            <Card sx={{ maxWidth: 345 }} >
-                    <CardContent>
-                    <Typography gutterBottom variant="h6" component="div" color="primary">
-                        S&P 500
-                    </Typography>
-                    <Typography variant="h5" color="txtPrimary">
-                        +{worth}
-                    </Typography>
-                    </CardContent>
-            </Card>
-            <br></br>
-            <Card sx={{ maxWidth: 345 }} >
-                    <CardContent>
-                    <Typography gutterBottom variant="h6" component="div" color="primary">
-                        Portoflio Performance
-                    </Typography>
-                    <Typography variant="h5" color="textPrimary" display="inline">
-                        {performance}%
-                    </Typography>
-                    </CardContent>
-            </Card>
-            <br></br>
-            <Card sx={{ maxWidth: 345 }} >
-                    <CardContent>
-                    <Typography gutterBottom variant="h6" component="div" color="primary">
-                        Highest Performing stock
-                    </Typography>
-                    <Typography variant="h5" color="textPrimary" display="inline">
-                        {astock}
-                    </Typography>
-                    <Typography color="textSecondary" display="inline">
-                        &ensp;+{percentage}%
-                    </Typography>
-                    </CardContent>
-            </Card>
-        </Grid>
-        <Grid item xs={9}>
+   
+    const [data,setData] = useState([])
+    //To log in as test user
+    useEffect(() => {
+        fetch('/portfolio/my_portfolio', {
+        method: 'POST',
+        headers: new Headers({
+            'Authorization': 'Bearer ' + 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY0Mzg3MDExOCwianRpIjoiOWQ2NDlhNDAtMzlhNi00NjU4LTliNmQtODdjYjgzNzI2MjAyIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6ImpvaG5AbWFpbC5jb20iLCJuYmYiOjE2NDM4NzAxMTgsImV4cCI6MTY0Mzg3MzcxOH0.ugi-qGJugz8es8h13ycwnhEu4oNjXRl2RHCTGB2ipvg'
+        }),
+        body: JSON.stringify({
+            
+        })
+        } ).then(
+        res => res.json()
+        ).then(
+            data => {
+                setData(data)
+            }
+        )
+        }, [])
         
+    const portfolioDict = data[0];
+
+    function createData(stock, amount, shares) {
+        return {stock, amount, shares};
+    }
+    const portfolio = [];
+
+    for (var key in portfolioDict){
+        portfolio.push(createData(key, portfolioDict[key]['amount'], portfolioDict[key]['shares']));
+    } 
+
+
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2} >
+            <Grid item xs={8}>
+                <Card >
+                    <CardContent>
+                    <Typography gutterBottom variant="h6" component="div" color="primary">
+                        Today
+                    </Typography>
+                    <ChartComponent />
+                    </CardContent>
+                </Card>
+            </Grid>
+            <Grid item xs={4}>
+                <Card sx={{ maxWidth: 345 }} >
+                        <CardContent>
+                        <Typography gutterBottom variant="h6" component="div" color="primary">
+                            S&P 500
+                        </Typography>
+                        <Typography variant="h5" color="txtPrimary">
+                            +{worth}
+                        </Typography>
+                        </CardContent>
+                </Card>
+                <br></br>
+                <Card sx={{ maxWidth: 345 }} >
+                        <CardContent>
+                        <Typography gutterBottom variant="h6" component="div" color="primary">
+                            Portoflio Performance
+                        </Typography>
+                        <Typography variant="h5" color="textPrimary" display="inline">
+                            {performance}%
+                        </Typography>
+                        </CardContent>
+                </Card>
+                <br></br>
+                <Card sx={{ maxWidth: 345 }} >
+                        <CardContent>
+                        <Typography gutterBottom variant="h6" component="div" color="primary">
+                            Highest Performing stock
+                        </Typography>
+                        <Typography variant="h5" color="textPrimary" display="inline">
+                            {astock}
+                        </Typography>
+                        <Typography color="textSecondary" display="inline">
+                            &ensp;+{percentage}%
+                        </Typography>
+                        </CardContent>
+                </Card>
+            </Grid>
+            <Grid item xs={9}>
+            
+                <TableContainer component={Paper}>
+                    <Table  aria-label="customized table">
+                        <TableHead>
+                        <Typography variant="h5" color="textPrimary" margin="10px">
+                            Portfolio
+                        </Typography>
+                        <TableRow>
+                            <StyledTableCell>Stock</StyledTableCell>
+                            <StyledTableCell align="center">Amount</StyledTableCell>
+                            <StyledTableCell align="right">Profit</StyledTableCell>
+                        </TableRow>
+                        </TableHead>
+                        <TableBody>
+                        {rows.map((row) => (
+                            <StyledTableRow>
+                            <StyledTableCell>
+                                {row.stock}
+                            </StyledTableCell>
+                            <StyledTableCell align="center">{row.amount}</StyledTableCell>
+                            <StyledTableCell align="right">{row.profit}%</StyledTableCell>
+                            </StyledTableRow>
+                        ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Grid>
+            <Grid item xs={3}>
             <TableContainer component={Paper}>
-                <Table  aria-label="customized table">
-                    <TableHead>
-                    <Typography variant="h5" color="textPrimary" margin="10px">
-                        Portfolio
-                    </Typography>
-                    <TableRow>
-                        <StyledTableCell>Stock</StyledTableCell>
-                        <StyledTableCell align="center">Amount</StyledTableCell>
-                        <StyledTableCell align="right">Profit</StyledTableCell>
-                    </TableRow>
-                    </TableHead>
-                    <TableBody>
-                    {rows.map((row) => (
-                        <StyledTableRow>
-                        <StyledTableCell>
-                            {row.stock}
-                        </StyledTableCell>
-                        <StyledTableCell align="center">{row.amount}</StyledTableCell>
-                        <StyledTableCell align="right">{row.profit}%</StyledTableCell>
-                        </StyledTableRow>
-                    ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+                    <Table  aria-label="customized table">
+                        <TableHead>
+                        <Typography variant="h5" color="textPrimary" margin="10px">
+                            Top Movers
+                        </Typography>
+                        <TableRow>
+                            <StyledTableCell>Stock</StyledTableCell>
+                            <StyledTableCell align="center">Amount</StyledTableCell>
+                            <StyledTableCell align="right">Profit</StyledTableCell>
+                        </TableRow>
+                        </TableHead>
+                        <TableBody>
+                        {portfolio.map((portfolio) => (
+                            <StyledTableRow>
+                            <StyledTableCell>
+                                {portfolio.stock}
+                            </StyledTableCell>
+                            <StyledTableCell align="center">{portfolio.amount}</StyledTableCell>
+                            <StyledTableCell align="right">{portfolio.shares}%</StyledTableCell>
+                            </StyledTableRow>
+                        ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Grid>
         </Grid>
-        <Grid item xs={3}>
-        <TableContainer component={Paper}>
-                <Table  aria-label="customized table">
-                    <TableHead>
-                    <Typography variant="h5" color="textPrimary" margin="10px">
-                        Top Movers
-                    </Typography>
-                    <TableRow>
-                        <StyledTableCell>Stock</StyledTableCell>
-                        <StyledTableCell align="center">Amount</StyledTableCell>
-                        <StyledTableCell align="right">Profit</StyledTableCell>
-                    </TableRow>
-                    </TableHead>
-                    <TableBody>
-                    {rows.map((row) => (
-                        <StyledTableRow>
-                        <StyledTableCell>
-                            {row.stock}
-                        </StyledTableCell>
-                        <StyledTableCell align="center">{row.amount}</StyledTableCell>
-                        <StyledTableCell align="right">{row.profit}%</StyledTableCell>
-                        </StyledTableRow>
-                    ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </Grid>
-      </Grid>
-    </Box>
-  );
+        </Box>
+    );
 }
