f7bb169e3e0d2e43ef766667cfeb3c64806ee28f
Mihika Marathe <mihika.marathe@gmail.com>
2022-02-07 16:27:01 -0800

    Delete config 2.js

--- frontend/src/components/StockPage/config 2.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  alpacas_api_key_id: "",
-  alpacas_api_secret_key:"",
-  base_url: "https://paper-api.alpaca.markets/",
-  Stock_url:"https://data.alpaca.markets"
-
-}
-export default config
