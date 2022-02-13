5db866c26c6d4b5e04afebba97ea5881c2fba612
Ojas Upadhye <ojasupadhye@gmail.com>
2022-02-13 11:13:50 -0800

    Revert "Delete App.js"
    This reverts commit 78e3c6aafa728006cfd55203895b3ba019faf30d.

---
+++ frontend/src/components/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import {Register} from "./login/index";
+import {Login} from "./login/index";
+import HomePage from './HomePage';
+
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path="/homepage" element={<HomePage/>} />
+          <Route path="/" element={<Login/>} />
+          <Route path="/register" element={<Register/>} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
