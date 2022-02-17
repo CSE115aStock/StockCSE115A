f2cee14f210f7813dc0133e3cf20e70c9866dea8
nreusken <nreusken@ucsc.edu>
2022-02-17 14:46:58 -0800

    Edite stockviewer page

--- frontend/src/components/StockViewer.js
+++ frontend/src/components/StockViewer.js
@@ -1,20 +1,4 @@
-import React from 'react';
-import StockViewerContext from './StockViewerContext';
-
-function StockViewer() {
-    return (
-        <StockViewerContext.Consumer>
-          {({finalSearch}) => (
-            <div>
-                Ticker: {finalSearch}
-            </div>
-          )}
-        </StockViewerContext.Consumer>
-    )
-
-}
-
-export default StockViewer;import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import StockViewerContext from './StockViewerContext';
 import Chart from './Charts/MACDchart';
 import { getData } from './Charts/utils';
